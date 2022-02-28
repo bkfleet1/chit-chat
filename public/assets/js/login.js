04ae609eb2e8567db3a69a1f6a80b21def8001f6
TelChance <chancetelford0018@gmail.com>
2022-02-28 17:50:45 -0600

    added login and logout

---
+++ public/assets/js/login.js
@@ -0,0 +1,50 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('').value.trim();
+    const password = document.querySelector('').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'apllication/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Can not log in.');
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = docment.querySelector('').value.trim();
+    const email = document.querySelector('').value.trim();
+    const password = document.querySelector('').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Can not sign up.')
+        }
+    }
+};
+
+document
+.querySelector('')
+.addEventListener('submit', loginFormHandler);
+
+document
+.querySelector('')
+.addEventListener('submit', signupFormHandler);
