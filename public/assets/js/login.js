8c1b09858e19963703012a35c5563503072fedb7
Chance Telford <chancetelford0018@gmail.com>
2022-02-28 20:18:09 -0600

    added signup.js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -19,32 +19,6 @@ const loginFormHandler = async (event) => {
     }
 };
 
-const signupFormHandler = async (event) => {
-    event.preventDefault();
-
-    const username = docment.querySelector('').value.trim();
-    const email = document.querySelector('').value.trim();
-    const password = document.querySelector('').value.trim();
-
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ username, email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('Can not sign up.')
-        }
-    }
-};
-
 document
 .querySelector('')
 .addEventListener('submit', loginFormHandler);
-
-document
-.querySelector('')
-.addEventListener('submit', signupFormHandler);
