40b8cecc1e439a8cb9a7290e807173f970fbf09a
bkfleet1 <bkfleet1@gmail.com>
2022-03-01 18:44:58 -0600

    Ratings CRUD endpoints working and tested

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userEndPoints = require('./user-routes.js');
 const shoutoutEndPoints = require('./shoutout-routes.js');
 const commentEndPoints = require('./comment-routes.js');
+const ratingEndPoints = require('./rating-routes.js');
 
 router.use('/users', userEndPoints);
 router.use('/shoutouts', shoutoutEndPoints);
 router.use('/comments', commentEndPoints);
+router.use('/ratings', ratingEndPoints);
 
 module.exports = router;
