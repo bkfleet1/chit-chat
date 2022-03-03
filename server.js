aaeae079f2f6c644f1d471be8ab6e5cb5f36faaf
TelChance <chancetelford0018@gmail.com>
2022-03-03 13:21:24 -0600

    got it to atleast post the picture

--- server.js
+++ server.js
@@ -7,22 +7,41 @@ const multer = require('multer');
 
 const app = express();
 
-const fileStorageEngine = multer.diskStorage ({
-  destination: (req, file, cb) => {
-    cb(null, 'image')
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "--" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: fileStorageEngine });
-
-app.post('/single', upload.single('image'), (req, res) => {
-  res.send("File upload success");
-});
-
 const PORT = process.env.PORT || 3001;
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
+
+
+app.use(express.static(__dirname + '/public'));
+app.use('/uploads', express.static('uploads'));
+
+app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
+console.log(JSON.stringify(req.file))
+var response = '<a href="/">Home</a><br>'
+response += "Files uploaded successfully.<br>"
+response += `<img src="${req.file.path}" /><br>`
+return res.send(response)
+})
+
+app.post('/profile-upload-multiple', upload.array('profile-files', 12), function (req, res, next) {
+  var response = '<a href="/">Home</a><br>'
+  response += "Files uploaded successfully.<br>"
+  for(var i=0;i<req.files.length;i++){
+      response += `<img src="${req.files[i].path}" /><br>`
+  }
+  
+  return res.send(response)
+})
+
+
 
 
 const sequelize = require("./config/connection");
