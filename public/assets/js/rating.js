99b0ab5cc5de6ec889c73a81acd74eff93c8b9c2
JJackson510 <jamiej.jj5@gmail.com>
2022-03-03 18:08:36 -0600

    rating

---
+++ public/assets/js/rating.js
@@ -0,0 +1,11 @@
+document.ready(function() {
+    $("form#ratingForm").submit(function(e) {
+    e.preventDefault(); 
+    if ($("#ratingForm :radio:checked").length == 0) {
+        document.getElementById("status").innerHTML = "nothing checked";
+        return false;
+    } else {
+        document.getElementById("status").innerHTML = 'You picked ' + $('input:radio[name=rating]:checked'.value);
+    }
+    });
+});
